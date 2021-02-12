a8c663779a7e45378aded4e87e5a364937e22ab7
Randy May <randy@mathysphere.com>
2021-02-12 15:52:55 -0500

    rework signal generator to use thread pools

--- jet-audio-monitor/src/main/java/com/sorintlab/tone/SignalSimulator.java
+++ jet-audio-monitor/src/main/java/com/sorintlab/tone/SignalSimulator.java
@@ -5,29 +5,19 @@ import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 import com.sorintlab.jet.data.acquisition.audio.AudioSample;
-
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.ShortBuffer;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class SignalSimulator {
-    private static final int SAMPLE_SECONDS = 5;
 
     private SineWave16Generator []generators;
-
-    
 
     public static void main(String[] args) {
         ArgumentParser parser = ArgumentParsers.newFor("SignalSimulator").build().defaultHelp(true)
@@ -44,7 +34,7 @@ public class SignalSimulator {
 
         boolean disableHazelcast = ns.getBoolean("disableHazelcast");
 
-        HazelcastInstance hz = null;
+        HazelcastInstance hz;
         IMap<Integer, AudioSample> map = null;
 
         if (!disableHazelcast){
@@ -60,85 +50,13 @@ public class SignalSimulator {
             e.printStackTrace(System.err);
             System.exit(1);
         }
-        int sampleRate = ss.generators[0].getSampleRate();
 
-        // NOTE: sample rate configuration should be the same for every generator
-
-        // now and n seconds after now, generate one second's worth of samples
-        // put those in a hz entry in a map
-        long nextWakeup = System.currentTimeMillis();
-        long sleepTime = 0;
-        ShortBuffer secondBuffer = ShortBuffer.allocate(sampleRate);
-
-        ByteBuffer[] exampleBuffers = new ByteBuffer[ss.generators.length];
-        for(int i=0;i < exampleBuffers.length; ++i){
-            exampleBuffers[i] = ByteBuffer.allocate(2 *sampleRate * SAMPLE_SECONDS);
-            exampleBuffers[i].order(ByteOrder.LITTLE_ENDIAN);
+        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(10);
+        for (SineWave16Generator generator: ss.generators){
+            generator.setMap(map);
+            executor.scheduleAtFixedRate(generator, 0, 1, TimeUnit.SECONDS);
         }
-
-        int s = 0;
-        while (true) {
-            long now = System.currentTimeMillis();
-            int buffer_num = 0;
-            AudioSample sample = null;
-            for (SineWave16Generator generator : ss.generators) {
-                secondBuffer.clear();
-                generator.writeSamples(secondBuffer, s);
-                if (s < SAMPLE_SECONDS)
-                    generator.writeSamples(exampleBuffers[buffer_num], s);
-
-                /*
-                 * Note that the array backing secondBuffer is reused. We can only get away with
-                 * this because the put sends the data to another process, effectively making a
-                 * copy. If the putAsync method is used, it will become necessary to copy the
-                 * byte array.
-                 */
-                sample = new AudioSample(buffer_num, now, secondBuffer.array());
-                if (map != null) map.put(sample.getId(), sample);
-
-                buffer_num += 1;
-            }
-
-            s += 1;
-            nextWakeup += 1000;
-            if (s == SAMPLE_SECONDS) {
-                buffer_num = 0;
-                for (ByteBuffer exampleBuffer : exampleBuffers) {
-                    exampleBuffer.flip();
-                    ByteArrayInputStream bis = new ByteArrayInputStream(exampleBuffer.array());
-                    AudioFormat audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, (float) sampleRate, 16,
-                            1, 2, (float) sampleRate, false);
-                    AudioInputStream ais = new AudioInputStream(bis, audioFormat, 5 * sampleRate);
-
-                    File outFile = new File("sample_" + buffer_num + ".wav");
-                    try {
-                        AudioSystem.write(ais, AudioFileFormat.Type.WAVE, outFile);
-                        System.out.println("SAMPLE WRITTEN TO " + outFile.getAbsolutePath());
-                    } catch (IOException iox) {
-                        iox.printStackTrace(System.err);
-                    }
-                    buffer_num += 1;
-                }
-            }
-
-            sleepTime = nextWakeup - System.currentTimeMillis();
-            if (sleepTime < -5000) {
-                System.err.println("cant't go that fast");
-                System.exit(1);
-            }
-
-            if (sleepTime > 0) sleep(sleepTime);
-        }
-
-    }
-
-    private static void sleep(long millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException ix) {
-            System.out.println("Interrupted - exiting");
-            System.exit(0);
-        }
+        System.out.println("Launched");
     }
 
     public SineWave16Generator[] getGenerators() {
@@ -148,4 +66,6 @@ public class SignalSimulator {
     public void setGenerators(SineWave16Generator[] generators) {
         this.generators = generators;
     }
+
+
 }
--- jet-audio-monitor/src/main/java/com/sorintlab/tone/SineWave16Generator.java
+++ jet-audio-monitor/src/main/java/com/sorintlab/tone/SineWave16Generator.java
@@ -1,17 +1,36 @@
 package com.sorintlab.tone;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.hazelcast.map.IMap;
+import com.sorintlab.jet.data.acquisition.audio.AudioSample;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.ShortBuffer;
+
 /**
  * Generates 16bit sine wave samples with configurable frequency, amplitude, phase and sample rate
  */
-public class SineWave16Generator {
+public class SineWave16Generator implements Runnable {
+    private static final int SAMPLE_SECONDS = 5;
+
     private int id;
     private short amplitude;
     private int   frequency;    // cycles per second
     private int sampleRate;     // samples per second
     private double phase;      //in radians
-    
+
+    private ByteBuffer  sampleBytes;
+    private ShortBuffer secondBuffer;
+    private int s;
+
     public int getId(){
         return id;
     }
@@ -68,7 +87,7 @@ public class SineWave16Generator {
     /**
      * Writes one second's worth of samples to the provided ByteBuffer
      * 
-     * @param the buffer to which samples will be written
+     * @param  buffer to which samples will be written
      * @param s identifies the particular second to write. s is external to the Generator so the same sample can be
      *          written multiple times (e.g. to different buffers)
      */
@@ -97,4 +116,51 @@ public class SineWave16Generator {
         double result = amplitude * val;
         return (short) result;
     }
+
+    @JsonIgnore
+    private IMap<Integer, AudioSample> map;
+
+    public void setMap(IMap<Integer, AudioSample> map){
+        this.map = map;
+        this.secondBuffer = ShortBuffer.allocate(sampleRate);
+        this.sampleBytes = ByteBuffer.allocate(sampleRate * SAMPLE_SECONDS * 2);
+        this.sampleBytes.order(ByteOrder.LITTLE_ENDIAN);
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("SENT: " + id);
+            secondBuffer.clear();
+            writeSamples(secondBuffer, s);
+            secondBuffer.flip();
+            AudioSample sample = new AudioSample(id, System.currentTimeMillis(), secondBuffer.array());
+            map.put(sample.getId(), sample);
+
+            if (s < SAMPLE_SECONDS) {
+                writeSamples(sampleBytes, s);
+            }
+
+            s += 1;
+
+            if (s == SAMPLE_SECONDS) {
+                sampleBytes.flip();
+                ByteArrayInputStream bis = new ByteArrayInputStream(sampleBytes.array());
+                AudioFormat audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, (float) sampleRate, 16,
+                        1, 2, (float) sampleRate, false);
+                AudioInputStream ais = new AudioInputStream(bis, audioFormat, 5 * sampleRate);
+
+                File outFile = new File("sample_" + id + ".wav");
+                try {
+                    AudioSystem.write(ais, AudioFileFormat.Type.WAVE, outFile);
+                    System.out.println("SAMPLE WRITTEN TO " + outFile.getAbsolutePath());
+                } catch (IOException iox) {
+                    iox.printStackTrace(System.err);
+                }
+            }
+        } catch(Exception x){
+            x.printStackTrace();
+        }
+    }
+
 }
